a6bdf3ce7f1909f9898eff00d0fb0e8d10e70150
Dave <daverdal@gmail.com>
2018-03-20 21:41:26 -0500

    Added permitted attributes to activeAdmin model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  resources :orders
+  resources :categorys
+  resources :order_items
+  resources :products
+  resources :provinces
+  resources :users
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
