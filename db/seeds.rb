74172ba2e5f57de21c8d73d1de8f423cf1a88391
Dave <daverdal@gmail.com>
2018-04-02 21:47:42 -0500

    Added Provinces to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+#Province.create(name: 'NWT', gstRate: 0.05, pstRate: 0.05, hstRate: 0)
+Province.create(name: 'Manitoba', gstRate: 0.05, pstRate: 0.05, hstRate: 0)
+Province.create(name: 'Saskatchewan', gstRate: 0.05, pstRate: 0.05, hstRate: 0)
+Province.create(name: 'Quebec', gstRate: 0, pstRate: 0, hstRate: 0.06)
+Province.create(name: 'Nova Scotia', gstRate: 0.03, pstRate: 0.05, hstRate: 0)
+Province.create(name: 'PEI', gstRate: 0.05, pstRate: 0.05, hstRate: 0)
+Province.create(name: 'New Brunswick', gstRate: 0.05, pstRate: 0.05, hstRate: 0)
+Province.create(name: 'Newfoundland', gstRate: 0.05, pstRate: 0.05, hstRate: 0)
